66bef770633871fabc99dbf5b31e652e2c44b71e
Tanmay Singhal <tanmay.singhal_cs.aiml24@gla.ac.in>
2026-02-20 10:32:05 +0530

    code for LDR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 /**
  * @file main.ino
  * @brief Embedded Light Intensity Monitoring using LDR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author TANMAY SINGHAL
+ * @date 2026-02-17
  *
  * @details
  * Reads analog brightness data from LDR sensor
@@ -12,33 +12,47 @@
 
  // TODO 1:
  // Define LDR analog pin (Use A0)
+ int ldrPin = A0;
 
  // TODO 2:
  // Create variable to store sensor reading
+ int ldrValue = 0;
 
 void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
     // TODO 4:
     // Print system initialization message
+    Serial.println("LDR Started");
 }
 
 void loop() {
 
     // TODO 5:
     // Read analog value from LDR
+    ldrValue = analogRead(ldrPin);
 
     // TODO 6:
     // Print raw ADC value
+    Serial.print("LDR Value: ");
+    Serial.println(ldrValue);
 
     // TODO 7:
     // Apply threshold logic (Bright / Dark detection)
+    if (ldrValue < 500) {
 
-    // TODO 8:
-    // Print brightness status
+        // TODO 8:
+        // Print brightness status
+        Serial.println("Bright Environment");
+    }
+    else {
+        Serial.println("Dark Environment");
+    }
 
     // TODO 9:
     // Add delay (500ms or 1 second)
+    delay(500);
 }
